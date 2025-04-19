cf8d96f98e7aab67f137343f2bbd631df08dfefd
bshar1865 <bsharesfke05@gmail.com>
2025-04-19 04:11:04 +0300

    updated idclass to match with main server

--- idclass.ts
+++ idclass.ts
@@ -1,7 +1,7 @@
 const idclass = {
-  roleDev: '1101061365692649532',
-  logChannel: '1183418783289856010',
-  channelErrorLogs: '1183418783289856010',
+  roleFA: '1032294674527948811',
+  logChannel: '1289697660638461952',
+  channelErrorLogs: '1362957145519427625',
 };
 
 export default idclass;
